04ca4bbc38ea03b71d832de03ff0cf8cea72a393
shubhambadade007 <93304777+shubhambadade007@users.noreply.github.com>
2023-04-08 09:38:19 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -8,14 +8,14 @@ java -version
 
 sudo apt update -y
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo apt update -y
-
-sudo add-apt-repository universe -y
-
+sudo apt-get update -y
 sudo apt-get install jenkins -y
 
 sudo service jenkins start
